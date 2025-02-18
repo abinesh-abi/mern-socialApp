a042f5096881eff921ccf94984524c1a52ee2309
abinesh-abi <abinesh1729@gmail.com>
2025-02-18 13:46:10 +0530

    feat: added env

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
-    SERVER_URL : process.env.REACT_APP_SERVER_URL,
+    SERVER_URL : process.env.REACT_APP_SERVER_URL || 'http://127.0.0.1:5000',
     CLIENT_URL : process.env.REACT_APP_CLIENT_URL,
     PEER_JS_URL: process.env.REACT_APP_PEER_JS_URL,
     PEER_JS_PORT: process.env.REACT_APP_PEER_JS_PORT,
